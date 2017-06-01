fd83aa692f05e310fcff6073ed70496b955c9ae1
veerdonk <davidvdveerdonk@hotmail.com>
2017-06-01 11:54:37 +0200

    changed heatmap button to datatables format, changed heatmapping function to accept qtl argument

--- src/main/resources/static/js/heatmap.js
+++ src/main/resources/static/js/heatmap.js
@@ -20,6 +20,36 @@ window.onload = function() {
                 extend: 'collection',
                 text: 'Export',
                 buttons: [ 'csv', 'excel', 'pdf', 'copy' ]
+            },
+            {
+                extend: 'collection',
+                text: 'Generate Heatmap',
+                buttons: [
+                    {
+                        text: 'Cytokines',
+                        action: function(){
+                            drawHeatmap('cytokine')
+                        }
+                    },
+                    {
+                        text: 'Cell Counts',
+                        action: function(){
+                            drawHeatmap('cellCount')
+                        }
+                    },
+                    {
+                        text: 'Hormones',
+                        action: function(){
+                            drawHeatmap('hormones')
+                        }
+                    },
+                    {
+                        text: 'Modulators',
+                        action: function(){
+                            drawHeatmap('immuneModulator')
+                        }
+                    }
+                ]
             }
         ],
         "ajax" : tableUrl,
@@ -30,9 +60,9 @@ window.onload = function() {
             {"data": "qtl_type"}
         ]
     });
-    $("div.toolbar").html('<div th:id="dropdownContainer" class="dropdown"> <button id="heatmapButton" class="dt-buttons">Generate Heatmap</button> <div class="dropdown-content"> <a href="#" th:id="cytokine" th:class="selectedQtl">Cytokines</a> <a href="#" th:id="cellCount" th:class="selectedQtl">Cell counts</a> <a href="#" th:id="hormones" th:class="selectedQtl">hormones</a> <a href="#" th:id="immuneModulator" th:class="selectedQtl">Immune modulators</a> </div> </div>');
-    $('.selectedQtl').click(function () {
-        var qtl = this.id;
+
+    function drawHeatmap(qtl){
+        console.log(qtl + ' clicked');
         console.log(qtl);
         if(region!=null){
             ajaxUrl = '/api/getHeatmapSnpFromDb?qtlId='+snpId+'&qtl='+qtl+'&region='+region;
@@ -97,7 +127,75 @@ window.onload = function() {
                 Plotly.newPlot('heatmap', [response[pos]], layout);
             })
         });
-    });
+    }
+    // $('.selectedQtl').click(function () {
+    //     console.log(this.id + ' clicked');
+    //     var qtl = this.id;
+    //     console.log(qtl);
+    //     if(region!=null){
+    //         ajaxUrl = '/api/getHeatmapSnpFromDb?qtlId='+snpId+'&qtl='+qtl+'&region='+region;
+    //         console.log(ajaxUrl);
+    //     }
+    //     else{
+    //         ajaxUrl = '/api/getHeatmapSnpFromDb?qtlId='+snpId+'&qtl='+qtl;
+    //     }
+    //     $.ajax({
+    //         url: ajaxUrl
+    //     }).then(function (response) {
+    //         console.log(response);
+    //         var data = [response[0]];
+    //         var h = data[0]["y"].length*17.5;
+    //         var w = data[0]["x"].length*20;
+    //
+    //         if(h<100){
+    //             h=100;
+    //         }
+    //
+    //         if(h>3500){
+    //             $('#tooMuchDataError').toggle();
+    //             h=3500;
+    //         }
+    //         if(w<600){
+    //             w=600;
+    //         }
+    //
+    //         var layout = {
+    //             margin: {
+    //                 l: 100,
+    //                 r: 100,
+    //                 b: 250,
+    //                 t: 20,
+    //                 pad: 4
+    //             },
+    //             height: h+250,
+    //             width: w
+    //
+    //         };
+    //         var pos = 0;
+    //         var max = response.length;
+    //         Plotly.newPlot('heatmap', [response[pos]], layout);
+    //         $('.hidden').addClass('pagebutton');
+    //         window.scrollTo(0,document.body.scrollHeight);
+    //         $('#next15').click(function () {
+    //             var r = $.Deferred();
+    //             pos += 1;
+    //             if(pos >= max-1){
+    //                 pos = max-1;
+    //             }
+    //             console.log([response[pos]]);
+    //             Plotly.newPlot('heatmap', [response[pos]], layout);
+    //             r.resolve;
+    //         });
+    //
+    //         $('#prev15').click(function () {
+    //             pos -= 1;
+    //             if(pos <= 0){
+    //                 pos = 0;
+    //             }
+    //             Plotly.newPlot('heatmap', [response[pos]], layout);
+    //         })
+    //     });
+    // });
 
 
 
